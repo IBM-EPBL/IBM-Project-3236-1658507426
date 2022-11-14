16ca4a41d71fdd07e169be53051a5e35a470c173
snehakarthika <88896204+jhombu@users.noreply.github.com>
2022-11-14 22:06:24 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 2/app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/baby')
+def baby():
+    return render_template('baby.html')
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=False,)
+    
